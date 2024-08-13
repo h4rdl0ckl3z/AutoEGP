33cbecbf6f3d227dc0e0459341887b3e63bbf98d
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-08-13 21:04:36 +0700

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -11,51 +11,56 @@
  Target Server Version : 101106 (10.11.6-MariaDB-0+deb12u1)
  File Encoding         : 65001
 
- Date: 13/08/2024 00:17:34
+ Date: 13/08/2024 21:02:38
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for egp
+-- Table structure for customers
 -- ----------------------------
-DROP TABLE IF EXISTS `egp`;
-CREATE TABLE `egp` (
-  `EGPID` int(11) NOT NULL AUTO_INCREMENT,
-  `numID` varchar(255) DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
-  `link` varchar(255) DEFAULT NULL,
-  `pubDate` varchar(255) DEFAULT NULL,
-  `pubt` varchar(255) DEFAULT NULL,
-  `pubd` varchar(255) DEFAULT NULL,
-  `pubm` varchar(255) DEFAULT NULL,
-  `puby` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`EGPID`,`link`) USING BTREE,
-  UNIQUE KEY `link` (`link`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
-
--- ----------------------------
--- Table structure for egp_id
--- ----------------------------
-DROP TABLE IF EXISTS `egp_id`;
-CREATE TABLE `egp_id` (
-  `EGPID` int(11) NOT NULL AUTO_INCREMENT,
-  `ename` varchar(255) DEFAULT NULL,
-  `numID` varchar(255) DEFAULT NULL,
-  `eaddress` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `ename` varchar(255) NOT NULL,
+  `epak` varchar(255) NOT NULL,
+  `ecity` varchar(255) NOT NULL,
+  `numid` varchar(255) NOT NULL,
+  `eaddress` text DEFAULT NULL,
   `etel` varchar(255) DEFAULT NULL,
   `efax` varchar(255) DEFAULT NULL,
-  `eweb` varchar(255) DEFAULT NULL,
-  `eEmail` varchar(255) DEFAULT NULL,
-  `eMap` varchar(255) DEFAULT NULL,
-  `eMassage` varchar(255) DEFAULT NULL,
-  `ecal` varchar(255) DEFAULT NULL,
-  `eper` varchar(255) DEFAULT NULL,
-  `esta` varchar(255) DEFAULT NULL,
-  `epak` varchar(255) DEFAULT NULL,
-  `ecity` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`EGPID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+  `eweb` text DEFAULT NULL,
+  `eemail` varchar(255) DEFAULT NULL,
+  `emap` text DEFAULT NULL,
+  `emassage` text DEFAULT NULL,
+  `ecal` text DEFAULT NULL,
+  `eper` text DEFAULT NULL,
+  `esta` int(11) DEFAULT NULL,
+  `exp` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for egps
+-- ----------------------------
+DROP TABLE IF EXISTS `egps`;
+CREATE TABLE `egps` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numID` int(11) DEFAULT NULL,
+  `title` mediumtext DEFAULT NULL,
+  `link` mediumtext DEFAULT NULL,
+  `pubDate` date DEFAULT NULL,
+  `pubt` int(11) DEFAULT NULL,
+  `pubd` int(11) DEFAULT NULL,
+  `pubm` int(11) DEFAULT NULL,
+  `puby` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `link` (`link`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
